0e55fce3b9a4e77f878bd0a352c5873d4a6a211c
lmcnelly990 <45598730+lmcnelly990@users.noreply.github.com>
2018-12-06 07:42:12 -0600

    Add files via upload

---
+++ MAZEBALLTHING.py
@@ -0,0 +1,17 @@
+Python 3.5.3 (default, Sep 27 2018, 17:25:39) 
+[GCC 6.3.0 20170516] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> from sense_hat import SenseHat
+>>> sense = SenseHat()
+>>> sense.clear()
+>>> r = (255,0,0)
+>>> b=(0,0,0)
+>>> maze = [[r,r,r,r,r,r,r,r],
+        [r,b,b,b,b,b,b,r],
+        [r,r,r,b,r,b,b,r],
+        [r,b,r,b,r,r,r,r],
+        [r,b,b,b,b,b,b,r],
+        [r,b,r,r,r,r,b,r],
+        [r,b,b,r,b,b,b,r],
+        [r,r,r,r,r,r,r,r]]
+>>> 
